3329ec8d3ce4a45a1fb2052062c2f5cf29cabe20
Christy Presler <cpresler@users.noreply.github.com>
2024-01-29 15:08:06 -0500

    add missing character from url validating regex

--- widget-src/utilities/Regexes.ts
+++ widget-src/utilities/Regexes.ts
@@ -1,4 +1,4 @@
-export const rxUrl = new RegExp(/^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/gi);
+export const rxUrl = new RegExp(/^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=!]*)$/gi);
 
 export const rxFigma = new RegExp(/figma\.com\/file\//);
 
