ae2eec9a6daa8da2560e56982130cad6fda84827
Ryan Srofe <ryan.srofe@nearform.com>
2025-01-15 11:23:40 -0500

    update regex validation (#94)

--- widget-src/utilities/Regexes.ts
+++ widget-src/utilities/Regexes.ts
@@ -1,5 +1,5 @@
 export const rxUrl = new RegExp(
-  /^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=!]*)$/gi,
+  /^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=!()\[\]$*',;]*)$/gi,
 );
 
 export const rxAsana = new RegExp(/asana\.com/);
